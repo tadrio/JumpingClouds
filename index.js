f01f4c2d997331c1a1239987ba54b519d27fbafe
Tadrio Tabosa <tadrio_88@hotmail.com>
2022-05-28 13:47:11 -0300

    Refactor and comment

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const c = [0,0,0,1,0,0];
+const c = [0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0];
 
 function jumpingOnClouds(array){
   let counter = 0
   for (let i = 0; i < array.length; i++) {
-    if (array[i + 2] == 0){
-      i++;
-      counter++;
-    } else if (array[i + 1] == 0){
+    if (array[i + 2] == 0){ //verifica se pode fazer um pulo duplo
+      i++; //faz o for pular uma iteração, uma vez que o pulo é duplo
+      counter++; //adiciona um ao contador de movimentos
+    } else if (array[i + 1] == 0){ //verifica se pode fazer um pulo simples
       counter++;
     }
   }
